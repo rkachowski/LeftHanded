9c5f76395481c392a1abe7343357acb179be9375
Donald Hutchison <git@toastymofo.net>
2015-02-28 15:04:03 +0100

    Gitignore.

---
+++ GameScene.h
@@ -0,0 +1,10 @@
+//
+// Created by Donald Hutchison on 28/02/15.
+// Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GameScene : NSObject
+@end
---
+++ GameScene.m
@@ -0,0 +1,13 @@
+//
+// Created by Donald Hutchison on 28/02/15.
+// Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "GameScene.h"
+
+
+@implementation GameScene
+{
+
+}
+@end
